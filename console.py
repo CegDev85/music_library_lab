e746b11b74d536ca217a5ea89c31bd84d92c327f
Codeclan Student <student@Codeclans-MacBook-Pro.local>
2021-05-25 16:13:54 +0100

    first artist added

--- console.py
+++ console.py
@@ -1,8 +1,13 @@
 import pdb
 from models.artist import Artist
+import repositories.artist_repository as artist_repository
+
 
 
 artist1 = Artist("Foo Fighters")
+artist_repository.save(artist1)
 
+artists_names = artist_repository.select_all()
+# print(artists_names)
 
 pdb.set_trace()
--- db/music_collection.sql
+++ db/music_collection.sql
@@ -1,13 +1,14 @@
-DROP TABLE IF EXISTS artist;
 DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS artist;
+
 
 CREATE TABLE albums (
-    id SERIAL PRIMARY KEY   ,
+    id SERIAL PRIMARY KEY,
     title VARCHAR(255),
     genre VARCHAR(255)
     );
 
-CREATE TABLE artist (
+CREATE TABLE artists (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255)
 );    
